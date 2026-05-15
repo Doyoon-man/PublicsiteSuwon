fdfc5c179e80ec4616f49ad1858f3a2eaa6f85f2
gimd10020 <gimd10020@gmail.com>
2026-05-15 15:25:12 +0900

    feat: add Footer component to public site layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,16 +13,16 @@ import site from "./../assets/site.svg"
 
 function Footer() {
   const menus = [
-    { title: '탑동IV', icon: topdong },
-    { title: '개발사업본부', icon: development },
-    { title: '강좌수강신청', icon: Sign },     
-    { title: '광교캠핑장', icon: camp },
-    { title: '수원연화장', icon: death },
-    { title: '종량제물품접수', icon: trash },
-    { title: '사업현황', icon: business },
-    { title: '온라인통합민원', icon: online },
-    { title: '전국 도시 공사', icon: city },
-    { title: '관련기관 사이트', icon: site }
+    { title: '탑동IV', url: 'https://www.tapdong.co.kr', icon: topdong },
+    { title: '개발사업본부', url: 'https://www.suwonudc.co.kr/swdevelop/mainPage.do', icon: development },
+    { title: '강좌수강신청', url: 'https://rsv.suwonudc.co.kr/suwonudc/uat/uia/LoginUsr.do', icon: Sign },     
+    { title: '광교캠핑장', url: 'https://www.suwonudc.co.kr/ggcamping/mainPage.do', icon: camp },
+    { title: '수원연화장', url: 'https://www.suwonudc.co.kr/suwonyhj/mainPage.do', icon: death },
+    { title: '종량제물품접수', url: 'https://www.suwonudc.co.kr/recycle/mainPage.do', icon: trash },
+    { title: '사업현황', url: 'https://www.suwonudc.co.kr/swdevelop/PageLink.do?thirdMenuNo=&subMenuNo=010600&menuN=010000&link=forward:/PageContent.do&tempParam1=&', icon: business },
+    { title: '온라인통합민원', url: 'https://www.suwonudc.co.kr/recycle/mainPage.do', icon: online },
+    { title: '전국 도시 공사', url: 'https://www.suwonudc.co.kr/swdevelop/mainPage.do', icon: city },
+    { title: '관련기관 사이트', url: 'https://www.suwonudc.co.kr/swdevelop/PageLink.do?thirdMenuNo=&subMenuNo=010600&menuN=010000&link=forward:/PageContent.do&tempParam1=&', icon: site }
   ];
 
   return (
@@ -31,7 +31,7 @@ function Footer() {
         <ul className="footer-menu-grid">
           {menus.map((menu, idx) => (
             <li key={idx}>
-              <a href="#" className="footer-link">
+              <a href={menu.url} target="_blank" rel="noopener noreferrer" className="footer-link">
                 <img src={menu.icon} className="footer-icon" aria-hidden="true" alt="바로가기" />
                 {menu.title}
               </a>
